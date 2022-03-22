4b98dce71dab258ea2b0782f3c2fc0ca1269244b
Inferno <43pradeep07@gmail.com>
2022-03-22 23:48:18 +0530

    String Shuffle

---
+++ 1528. Shuffle String/shufflingstring.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        int n = s.size();
+        string res = s;
+        for(int i=0; i<n; i++)
+            res[indices[i]] = s[i];
+        return res;
+    }
+};
