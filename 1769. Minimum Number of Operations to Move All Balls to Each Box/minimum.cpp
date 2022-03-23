250784d43752a470be8acc4184130af0c39d5e4a
Inferno <43pradeep07@gmail.com>
2022-03-24 00:02:48 +0530

    Redo

--- 1769. Minimum Number of Operations to Move All Balls to Each Box/minimum.cpp
+++ 1769. Minimum Number of Operations to Move All Balls to Each Box/minimum.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
         int n = boxes.size();
-        vector<int> ans;
-        for(int i=0; i<n; i++)
+        vector<int> ans(n);
+        
+        for (int i = 0, count = 0, move = 0; i < boxes.length(); ++i)
         {
-            int res = 0;
-            for(int j=0; j<n; j++)
-            {
-                if(boxes[j] == '1')
-                    res += abs(j-i);
-            }
-            ans.push_back(res);
+            ans[i] += move;
+            count += boxes[i] - '0';
+            move += count;
+        }
+        
+        for (int i = boxes.length() - 1, count = 0, move = 0; i >= 0; --i)
+        {
+            ans[i] += move;
+            count += boxes[i] - '0';
+            move += count;
         }
         
         return ans;
